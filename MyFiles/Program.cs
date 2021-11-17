4a6dd5538ac5d2926ea9b9703669e0183d669ddb
Opilane TTHK <opilane@tthk.ee>
2021-11-17 12:52:23 +0200

    first commit

---
+++ MyFiles/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+namespace MyFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullFilePath = @"C:\Users\opilane\Samples\TITpe21";
+            string filePath = fullFilePath + @"\MyFiles.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            for (int i = 0; i < dataFromFile.Length; i++)
+            {
+                dataFromFile[i] = dataFromFile[i].Replace('4', 'a');
+                dataFromFile[i] = dataFromFile[i].Replace('3', 'e');
+                dataFromFile[i] = dataFromFile[i].Replace('0', 'o');
+                dataFromFile[i] = dataFromFile[i].Replace('1', 'i');
+
+                File.WriteAllLines(filePath, dataFromFile);
+            }
+            foreach (string line in dataFromFile)
+            {
+                File.Create($@"{fullFilePath}\{line}.txt");
+            }
+        }
+    }
+}
---
+++ RandomQuote/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+namespace RandomQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("49 Wise Quotes That Will Inspire You to Success in Life");
+            Console.WriteLine(GetRandomQuote());
+        }
+
+
+        public static string GetRandomQuote()
+        {
+            string fullfilepath = @"C:\Users\opilane\Samples\quotes.txt";
+            string[] RandomQuote = File.ReadAllLines(fullfilepath);
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, RandomQuote.Length);
+
+            return (RandomQuote[randomIndex]);
+        }
+    }
+}
